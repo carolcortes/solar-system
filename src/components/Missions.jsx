70deb0120907f457874dc6abdc4d471c25432aa2
carolcortes <caroline.ocortes@gmail.com>
2022-06-22 12:27:33 -0300

    finalizado

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -8,14 +8,16 @@ class Missions extends React.Component {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
-        {missions.map(({ name, year, country, destination }) => (
-          <MissionCard
-            key={ name }
-            name={ name }
-            year={ year }
-            country={ country }
-            destination={ destination }
-          />))}
+        <div className="mission-card">
+          {missions.map(({ name, year, country, destination }) => (
+            <MissionCard
+              key={ name }
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+            />))}
+        </div>
       </div>
     );
   }
